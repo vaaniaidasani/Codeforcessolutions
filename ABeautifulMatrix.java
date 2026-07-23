01e439b491896f6bc4d9c7b49d452030b9ecd73a
vaaniaidasani <vaaniaidasani@gmail.com>
2026-07-23 12:31:04 +0530

    Initial Codeforces solutions

---
+++ ABeautifulMatrix.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ABeautifulMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int row = 0, col = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                int x = sc.nextInt();
+                if (x == 1) {
+                    row = i;
+                    col = j;
+                }
+            }
+        }
+
+        System.out.println(Math.abs(row - 3) + Math.abs(col - 3));
+    }
+}
---
+++ ADivisibilityProblem.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ADivisibilityProblem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            if (a % b == 0) {
+                System.out.println(0);
+            } else {
+                System.out.println(b - (a % b));
+            }
+        }
+
+        sc.close();
+    }
+}
