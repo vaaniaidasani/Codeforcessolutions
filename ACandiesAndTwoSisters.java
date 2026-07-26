e366e9dbb2e16666c3c08f6256da961f0a6f1e44
vaaniaidasani <vaaniaidasani@gmail.com>
2026-07-26 20:43:29 +0530

    Add ACandiesAndTwoSisters

---
+++ ACandiesAndTwoSisters.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ACandiesAndTwoSisters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            long n = sc.nextLong();
+
+            if (n <= 2) {
+                System.out.println(0);
+            } else if (n % 2 == 0) {
+                System.out.println((n - 2) / 2);
+            } else {
+                System.out.println(n / 2);
+            }
+        }
+
+        sc.close();
+    }
+}
