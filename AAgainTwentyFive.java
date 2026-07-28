a453d457bc3a76284a2321ebfc7e0df96defbcd2
vaaniaidasani <vaaniaidasani@gmail.com>
2026-07-28 12:00:07 +0530

    Add AAgainTwentyFive

---
+++ AAgainTwentyFive.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class AAgainTwentyFive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.nextLong(); // Read n, but it is not needed
+        System.out.println(25);
+        sc.close();
+    }
+}
