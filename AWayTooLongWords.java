5cb695cdda6d758e082df93080979de2d1da9f68
vaaniaidasani <vaaniaidasani@gmail.com>
2026-08-04 12:06:55 +0530

    AWayTooLongWords

---
+++ AWayTooLongWords.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class AWayTooLongWords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        while (n-- > 0) {
+            String word = sc.next();
+
+            if (word.length() > 10) {
+                System.out.println(word.charAt(0) + "" + (word.length() - 2) + word.charAt(word.length() - 1));
+            } else {
+                System.out.println(word);
+            }
+        }
+
+        sc.close();
+    }
+}
