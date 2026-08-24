08b76b0f5a126ea763039d34c5530458f3188bcb
vaaniaidasani <vaaniaidasani@gmail.com>
2026-08-24 22:18:58 +0530

    ADominoPiling

---
+++ ADominoPiling.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class ADominoPiling {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+
+        int ans = (M * N) / 2;
+
+        System.out.println(ans);
+    
+    sc.close();
+    }
+}
