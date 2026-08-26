5226721cf781d3f5228d06b74cd355fe7d9898ec
vaaniaidasani <vaaniaidasani@gmail.com>
2026-08-26 20:13:37 +0530

    AGameWithIntegers

---
+++ AGameWithIntegers.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class AGameWithIntegers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+
+            if (n % 3 == 0) {
+                System.out.println("Second");
+            } else {
+                System.out.println("First");
+            }
+        }
+
+        sc.close();
+    }
+}
